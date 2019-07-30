f08ab75846abf5cdcc444dab4104160bc300eb6d
Lasith <m.lasith.c@gmail.com>
2019-07-30 08:37:47 +0530

    Add new port

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const pg = require('pg');
 const cors = require('cors');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 //import JS modules
 var superAdmin = require('../src/Component/Users/BEnd/SuperAdmin/SuperAdmin');
