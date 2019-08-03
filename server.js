4e0b28bb59627281cd15938079c5d483931626f7
Lasith <m.lasith.c@gmail.com>
2019-08-03 07:32:52 +0530

    Now ok

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 var inDev = process.env.NODE_ENV !== 'production';
 var PORT = inDev ? 8080 : process.env.PORT;
 const path = require('path');
-const targetBaseUrl = 'http://localhost:3000/';
+const targetBaseUrl = ' https://money360.herokuapp.com/';
 
 //import JS modules
 var superAdmin = require('./src/Component/Users/BEnd/SuperAdmin/SuperAdmin');
